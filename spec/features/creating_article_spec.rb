f071ba214518d4de722e48f20bacaf6452359c85
BluePhoenixRa <r.arefin@gmail.com>
2015-10-18 14:37:11 -0400

    Finished testing creation of new article. Works as intended.

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -4,13 +4,13 @@ RSpec.feature "Creating New Posts" do
   scenario "User creates new blog post" do
     visit "/"
 
-    click_link "New Post"
+    click_link "New Article"
 
     fill_in "Title", with: "First Article"
     fill_in "Body", with: "This is body of the first article."
-    click_button "Add Post"
+    click_button "Create Article"
 
-    expect(page).to have_content("New blog post added")
+    expect(page).to have_content("New article has been created")
     expect(page.current_path).to eq(articles_path)
   end
 end
