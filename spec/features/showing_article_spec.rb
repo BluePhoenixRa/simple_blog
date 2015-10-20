57c34860831168ee674a827e08192e1f7d834b7e
BluePhoenixRa <r.arefin@gmail.com>
2015-10-20 00:01:14 -0400

    Updated User restrictions

--- spec/features/showing_article_spec.rb
+++ spec/features/showing_article_spec.rb
@@ -2,7 +2,43 @@ require 'rails_helper'
 
 RSpec.feature "Show Article" do
   before do
-    @article = Article.create(title: "Alpha Article", body: "Body of alpha article")
+    @jane = User.create(email: "jane@test.com", password: "password")
+    @bob = User.create(email: "bob@bob.com", password: "password")
+    @article = Article.create(title: "Alpha Article", body: "Body of alpha article", user: @jane)
+  end
+
+  scenario "Owner of Article will see Edit and Delete buttons" do
+    login_as(@jane)
+
+    visit "/"
+
+    click_link @article.title
+
+    expect(page).to have_link("Edit Article")
+    expect(page).to have_link("Delete Article")
+  end
+
+  scenario "Users must log in to see Edit or Delete buttons" do
+    visit "/"
+
+    click_link @article.title
+
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
+  end
+
+  scenario "If Article doesn't belong to User, they should not see Edit or Delete buttons" do
+    login_as(@bob)
+
+    visit "/"
+
+    click_link @article.title
+
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
   end
 
   scenario "Display individual article" do
